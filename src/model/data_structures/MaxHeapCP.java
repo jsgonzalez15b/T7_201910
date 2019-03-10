e1cc05f50025de02cc30700c67c5ec50838686a9
jsgonzalez15b <47197962+jsgonzalez15b@users.noreply.github.com>
2019-03-10 10:45:40 -0500

    Update MaxHeapCP.java
    Primer actualización, solo tres atributos y el método constructor

--- src/model/data_structures/MaxHeapCP.java
+++ src/model/data_structures/MaxHeapCP.java
@@ -1,5 +1,33 @@
 package model.data_structures;
+/**
+ *Heap de prioridad siempre ordenado de mayor a menor
+ */
+public class MaxHeapCP <T extends Comparable<T>>
+{
+  //Atributos
 
-public class MaxHeapCP <T extends Comparable<T>>{
-
+	/**
+	 * Nodo maximo de Heap, primer nodo
+	 */
+	public Nodo<T> maximo;
+	
+	/**
+	 * numero de elementos en Heap
+	 */
+	public int cantidad;
+	
+  /**
+	 * altura actual del Heap
+	 */
+	public int generacion;
+  
+	//Metodo Constructor
+	public MaxHeapCP ()
+	{
+		//Inicializa el maximo nodo como vacio, y la cantidad y altura como 0.
+		maximo= null;
+		altura = 0;
+		cantidad= 0;
+	}
+  
 }
